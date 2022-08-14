f6171e05c3b29b650eab9be781411300db813822
Viktor <Lumes119@gmail.com>
2022-08-14 19:11:35 +0300

    Created gane logic in utils.py, bullscows view for game handling and templates index and bullscows

---
+++ bullscows/game/utils.py
@@ -0,0 +1,52 @@
+from random import sample
+
+def create_answer():
+    number_list = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
+    answer = ''.join(sample(number_list, 4))
+    return answer
+
+def verify_try(player_try):
+    if len(player_try) == 4 and len(set(player_try)) == 4:
+        for i in player_try:
+            if i not in ('1', '2', '3', '4', '5', '6', '7', '8', '9'):
+                return False
+        return True
+    return False
+
+def check_try(answer, player_try):
+    bulls=0
+    cows = 0
+    for i in range(len(player_try)):
+        if player_try[i] == answer[i]:
+            cows+=1
+            bulls+=1
+        elif player_try[i] in answer:
+            cows+=1
+    return str(cows), str(bulls)
+
+def get_try_string(answer, player_try, try_string=None):
+    tuple_cowsbulls = check_try(answer, player_try)
+    if not try_string:
+        try_string = f'{player_try}  {tuple_cowsbulls[0]}:{tuple_cowsbulls[1]}'
+        return try_string
+    else:
+        try_string += f',{player_try}  {tuple_cowsbulls[0]}:{tuple_cowsbulls[1]}'
+    return  try_string
+
+
+
+
+if __name__ == '__main__':
+    a = create_answer()
+    print(a)
+    try_string = ""
+    player_try = "5678"
+    try_string= get_try_string(a, player_try, try_string)
+    print(try_string)
+    player_try1 = "3456"
+    try_string = get_try_string(a, player_try1, try_string)
+    print(try_string)
+    try_list = try_string.split(',')
+    print(try_list)
+
+
--- bullscows/game/views.py
+++ bullscows/game/views.py
@@ -1,13 +1,57 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Bullscows
+from .utils import create_answer, check_try, verify_try, get_try_string
+from django.contrib.auth.models import User
 
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    a = request.GET.get('name')
+    return render(request, 'game\index.html', {'a':a})
 
 def bullscows(request):
-    pass
+
+    player = request.user
+    print(player)
+
+    """creating a new gamepage  for user"""
+    if player.game.count()==0:
+        print('1')
+        answer = create_answer()
+
+        game = Bullscows.objects.create(answer=answer, user=player)
+        game.save()
+        new_game = 'Новая игра'
+        return render(request, 'game\\bullscows.html', context={'new_game': new_game})
+    game = player.game.all()[0]
+    data= {}
+    """part for give-up player's decision"""
+    print(game.answer)
+    if request.method == 'POST':
+        print('2')
+        if request.POST.get('value') =='give up':
+            print('3')
+            answer = f'Загаданное число - {game.answer}'
+            list_of_tries = game.try_string.split(',')
+            game.delete()
+            return render(request, 'game\\bullscows.html', context={'answer':answer, 'list_of_tries':list_of_tries})
+        else:
+            player_try = request.POST.get('name')
+            if not verify_try(player_try):
+                list_of_tries = game.try_string.split(',')
+                warning = "Число должно содержать 4 неповторяющиеся цифры в диапазоне 1-9"
+                return render(request, 'game\\bullscows.html', context={'warning':warning,
+                                                                        'list_of_tries':list_of_tries})
+            game.try_string = get_try_string(game.answer, player_try, game.try_string)
+            game.save()
+            data['list_of_tries'] = game.try_string.split(',')
+            tuple_cowsbulls = check_try(game.answer, player_try)
+            if tuple_cowsbulls[1] == '4':
+                data['answer'] = f"Вы вычислили загаданное число - {game.answer}. Поздравляем"
+                game.delete()
+    return render(request, 'game\\bullscows.html', context=data)
+
+
